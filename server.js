9866cce7a1233cf1180f4a62905160e135fa7dfb
bhagyeshbhavsar <bhavsarbhagyesh70@gmail.com>
2022-02-05 15:18:38 -0500

    Server page update

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ app.use( express.static( "public" ) );
 
 // index page
 app.get('/', function(req, res) {
-    var mascots = [
-      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-      { name: 'Tux', organization: "Linux", birth_year: 1996},
-      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-  
-    res.render('pages/index', {
-      mascots: mascots,
-      tagline: tagline
+     res.render('pages/index', {
     });
   });
 
